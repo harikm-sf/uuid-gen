f78063e11360a555ce25249509701cb7df54ee90
hm <hm@192.168.1.102>
2018-05-16 20:37:56 +0530

    Added Character encoding

--- src/main/java/io/digicom/core/uuid/controller/UUIDController.java
+++ src/main/java/io/digicom/core/uuid/controller/UUIDController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.digicom.core.uuid.model.UUIDModel;
@@ -18,12 +19,14 @@ public class UUIDController  {
 	@Autowired
 	UUIDFactory uuidFactory;
 	
-	@RequestMapping(value = "/uuid", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/uuid", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	@ResponseBody
 	public List<UUIDModel> getUUID() throws UnsupportedEncodingException {
 		return uuidFactory.getOne();
 	}
 	
-	@RequestMapping(value = "/uuid/{num}", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/uuid/{num}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	@ResponseBody
 	public List<UUIDModel> getUUID2(@PathVariable(value="num") int num) throws UnsupportedEncodingException {
 		return uuidFactory.getMany(num);
 	}
